15aa60823c0ce358ba3b4735d2c037ef8233b0b7
mmvergara <ymark.jesusmanabat@gmail.com>
2023-12-20 08:54:09 +0800

    update

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,13 +1,15 @@
 <template>
-  <nav>
-    
-  </nav>
+  <nav class="app-sidebar-container">q</nav>
 </template>
 
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <style scoped>
-
+.app-sidebar-container {
+  width: 20rem;
+  padding: 2rem;
+  background-color: gray;
+  border-right: 1px solid var(--color-gray-200);
+  overflow-y: auto;
+}
 </style>
