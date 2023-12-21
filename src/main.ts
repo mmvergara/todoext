7dd6d64f8353d96313fcebe6a761ff1f611b5710
mmvergara <ymark.jesusmanabat@gmail.com>
2023-12-22 06:16:54 +0800

    added-toastify

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import "./assets/main.css";
+import Vue3Toastify, { type ToastContainerOptions } from "vue3-toastify";
 
 import { createApp } from "vue";
 import App from "./App.vue";
@@ -8,5 +9,8 @@ import { store, storeKey } from "./store";
 const app = createApp(App);
 app.use(store, storeKey);
 app.use(router);
+app.use(Vue3Toastify, {
+  autoClose: 1500,
+} as ToastContainerOptions);
 
 app.mount("#app");
